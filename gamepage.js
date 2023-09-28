67a6273f1bd9e04403176355cd853296a114d5d2
Akshatjiwtode <133693819+Akshatjiwtode@users.noreply.github.com>
2023-09-28 18:52:12 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,29 @@
+// generation of random value
+var y = Math.floor(Math.random() * 10 + 1);
+
+
+ guess = 1;
+  function submit(){
+    var x = document.getElementById("guessField").value;
+  
+
+  if(x == y)
+  {
+    alert("CONGRATULATIONS!!! YOU GUESSED "+ guess +" IT RIGHT "  );
+    guess = 1;
+  }
+  else if(x>y)
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A GREATER NUMBER");
+}
+  }
+function playAgain(){
+    y = Math.floor(Math.random() * 10 + 1);
+}
+  
